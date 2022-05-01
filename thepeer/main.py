46db141db2c4e05db4f783561b1703b08bc2e3e8
E-wave112 <osagieiyayi09@gmail.com>
2022-05-01 10:28:30 +0000

    added hooks

--- thepeer/main.py
+++ thepeer/main.py
@@ -1,11 +1,11 @@
-import requests
-import httpx
-import hmac
 import hashlib
+import hmac
+import json  # type: ignore
+
+import httpx
 from decouple import config  # type: ignore
 from utils.constants import BASE_URL  # type: ignore
 from utils.exceptions.handleErrors import SwitchErrorStates  # type: ignore
-import json # type: ignore
 
 
 class ThePeerInit:
@@ -17,7 +17,8 @@ class ThePeerInit:
         self.headers = {"x-api-key": self.secret, "content-Type": "application/json"}
 
     def validate_signature(self, data, signature):
-        """helper method to validate the signature of the data received from thepeer's servers
+        """helper method to validate the signature of the data received
+        from thepeer's servers
         Args:
             data (dict|Any): the payload to which the signature is applied
             signature (string): the signature to be validated
@@ -26,31 +27,83 @@ class ThePeerInit:
             _type_: boolean if the signature is valid or not
         """
         return (
-            signature
-            == hmac.new(self.secret.encode(), data.encode(), hashlib.sha1()).hexdigest()
+            signature == hmac.new(self.secret.encode(), data.encode(), hashlib.sha1()).hexdigest()
         )
 
     def index_user(self, name, identifier, email):
+
+        """this method helps identify the user on thepeer's servers in order to facilitate
+        more usage of the SDK
+        it is usually the first method called by the user
+
+        Args:
+            name (string): the name of the registered user
+            identifier (string): the universal identifier of the registered user
+            email (string): the email of the registered user
+
+        Returns:
+            dict: the json response from the server containing the user's id and
+            other related information
+        """
         try:
-            """this method helps identify the user on thepeer's servers in order to facilitate more usage of the SDK
-            it is usually the first method called by the user
 
-            Args:
-                name (string): the name of the registered user
-                identifier (string): the universal identifier of the registered user
-                email (string): the email of the registered user
-
-            Returns:
-                dict: the json response from the server containing the user's id and other related information
-            """
             data = {"name": name, "identifier": identifier, "email": email}
             # convert the data to json
             data = json.dumps(data)
-            response = httpx.post(
-                f"{self.url}/users", data=data, headers=dict(self.headers)
-            )
+            response = httpx.post(f"{self.url}/users", data=data, headers=dict(self.headers))
             return response.json()
 
+        except Exception as e:
+            raise SwitchErrorStates(e).switch()
+
+    def view_user(self, reference):
+        """this method helps view the user's information on thepeer's servers
+        it is usually called after the user has indexed himself
+
+        Args:
+            reference (string): the reference of the indexed user
+
+        Returns:
+            dict: the json response from the server containing the user's id and
+            other related information
+        """
+        try:
+            response = httpx.get(f"{self.url}/users/{reference}", headers=dict(self.headers))
+            return response.json()
+        except Exception as e:
+            raise SwitchErrorStates(e).switch()
+
+    def update_user(self, reference, data):
+        """this method helps update the user's information on thepeer's servers
+        it is usually called after the user has indexed himself
+
+        Args:
+            reference (string): the reference of the indexed user
+            data (dict): the data to be updated
+
+        Returns:
+            dict: the json response from the server containing the user's id and
+            other related information
+        """
+        try:
+            data = json.dumps(data)
+            response = httpx.put(
+                f"{self.url}/users/{reference}", data=data, headers=dict(self.headers)
+            )
+            return response.json()
+        except Exception as e:
+            raise SwitchErrorStates(e).switch()
+
+    def delete_user(self, reference):
+        """this method helps delete the indexed user info on thepeer's servers
+        a user can always reindex himself
+
+        Args:
+            reference (string): the reference of the indexed user
+        """
+        try:
+            response = httpx.delete(f"{self.url}/users/{reference}", headers=dict(self.headers))
+            return response.json()
         except Exception as e:
             raise SwitchErrorStates(e).switch()
 
@@ -58,4 +111,5 @@ class ThePeerInit:
 # test function
 thepeer = ThePeerInit(config("PEER_SECRET_KEY"))
 test = thepeer.index_user("Osagie Iyayi", "iyayiemmanuel1@gmail.com", "iyayiemmanuel1@gmail.com")
+get = thepeer.delete_user("e09c7080-acd9-452d-b3e4-83902fc1368b")
 print(test)
--- thepeer/utils/exceptions/handleErrors.py
+++ thepeer/utils/exceptions/handleErrors.py
@@ -1,10 +1,10 @@
-from .types.notfound import NotFoundException  # type: ignore
-from .types.notacceptable import NotAcceptableException  # type: ignore
 from .types.forbidden import ForbiddenException  # type: ignore
+from .types.notacceptable import NotAcceptableException  # type: ignore
+from .types.notfound import NotFoundException  # type: ignore
+from .types.servererror import ServerErrorException  # type: ignore
+from .types.serviceunavailable import ServiceUnavailableException  # type: ignore
 from .types.unauthorized import UnauthorizedException  # type: ignore
 from .types.unprocessableentity import UnprocessableEntityException  # type: ignore
-from .types.serviceunavailable import ServiceUnavailableException  # type: ignore
-from .types.servererror import ServerErrorException  # type: ignore
 
 
 class SwitchErrorStates:
@@ -19,16 +19,16 @@ class SwitchErrorStates:
         status = self.error.response.status
         error_message = self.error.response.data.message
         if status == 401:
-            raise UnauthorizedException({message: error_message})
+            raise UnauthorizedException({"message": error_message})
         elif status == 403:
-            raise ForbiddenException({message: error_message})
+            raise ForbiddenException({"message": error_message})
         elif status == 404:
-            raise NotFoundException({message: error_message})
+            raise NotFoundException({"message": error_message})
         elif status == 406:
-            raise NotAcceptableException({message: error_message})
+            raise NotAcceptableException({"message": error_message})
         elif status == 422:
-            raise UnprocessableEntityException({message: error_message})
+            raise UnprocessableEntityException({"message": error_message})
         elif status == 500:
-            raise ServerErrorException({message: error_message})
+            raise ServerErrorException({"message": error_message})
         elif status == 503:
-            raise ServiceUnavailableException({message: error_message})
+            raise ServiceUnavailableException({"message": error_message})
